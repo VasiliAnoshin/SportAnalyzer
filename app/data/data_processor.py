f4d3117ea5a7125a33ba7281c12d5a39709cb999
BenjaminFranklin <anoshin45@gmail.com>
2023-12-30 22:47:41 +0200

    data_processor

--- app/data/data_processor.py
+++ app/data/data_processor.py
@@ -5,7 +5,17 @@ class DataProcessor:
     def __init__(self, all_games_df:pd.DataFrame) -> None:
         self.games_df= all_games_df
 
-    def get_list_of_games(self):
+    def get_list_of_games(self)->list:
+        """
+        Get a list of unique sports from the DataFrame.
+
+        Returns:
+        - list: A list containing unique sports extracted from the "key" column.
+
+        Example:
+        >>> print(sports_list)
+        ['Volleyball', 'Tennis', 'Soccer', ...]
+        """
         output = set()
         for game in self.games_df["key"]:
             res = game.partition("/")[0]
@@ -13,17 +23,22 @@ class DataProcessor:
                 output.add(res)
         return output
 
-    def get_games_count_per_sport(self) ->dict:
+    def get_games_count_per_sport(self, sport:str) ->dict:
         try:
-            game_col = defaultdict(int)
+            count = 0
             for game_entry in self.games_df["key"]:
                 game = game_entry.partition("/")[0]
-                if game != "":
-                    game_col[game] +=1
-            return game_col
+                if game != "" and game == sport:
+                    count+=1
+            if count == 0:
+                raise KeyError(f"There is no such sport -[ {sport} ]. Try to select available sport from list_all_games endpoint.")
+            return count
         except AttributeError as e:
             print(f"Check input data: dataframe or [key] header is not available: {e}")
             raise
         except IndexError as e:
             print(f"Can't split input_data entry, partition returns fewer than 3 parts: {e}")
             raise
+
+    def _contain_sport(self, sport)-> bool:
+        return sport in self.get_list_of_games()
